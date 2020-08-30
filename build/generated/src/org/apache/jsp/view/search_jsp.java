501d4a471de821119136f5b189500e7bbff956d8
Kaito Capricornus <40046355+KaitoCapricornus@users.noreply.github.com>
2020-08-30 16:04:27 +0700

    Add files via upload
    thuc commit

---
+++ build/generated/src/org/apache/jsp/view/search_jsp.java
@@ -0,0 +1,93 @@
+package org.apache.jsp.view;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class search_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>Search Book</title>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("    <center>\n");
+      out.write("        <h1>Search</h1>\n");
+      out.write("        <form>\n");
+      out.write("            <table>\n");
+      out.write("                <tr>\n");
+      out.write("                    <td>Book name</td>\n");
+      out.write("                    <td><input type=\"text\" name=\"BookName\"/></td>\n");
+      out.write("                </tr>\n");
+      out.write("                <tr>\n");
+      out.write("                    <td>Type</td>\n");
+      out.write("                    <td>\n");
+      out.write("                        <input type=\"checkbox\" name=\"types\" value=\"Action\"/>Action\n");
+      out.write("                        <input type=\"checkbox\" name=\"types\" value=\"Isekai\"/>Action\n");
+      out.write("                        <input type=\"checkbox\" name=\"types\" value=\"Romance\"/>Action\n");
+      out.write("                        <input type=\"checkbox\" name=\"types\" value=\"Novel\"/>Action\n");
+      out.write("                        <input type=\"checkbox\" name=\"types\" value=\"Comedy\"/>Action\n");
+      out.write("                    </td>\n");
+      out.write("                </tr>\n");
+      out.write("                <tr>\n");
+      out.write("                    <td></td>\n");
+      out.write("                    <td><input type=\"submit\" value=\"Search\"/></td>\n");
+      out.write("                </tr>\n");
+      out.write("            </table>\n");
+      out.write("        </form>\n");
+      out.write("    </center>\n");
+      out.write("</body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
